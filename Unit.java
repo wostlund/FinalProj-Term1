6109127f0736b5f02daaa8dcfd05aaa60efc3713
Jackson Morgan <jmorgan@stuy.edu>
2016-01-07 13:18:05 -0500

    created set functions

--- Unit.java
+++ Unit.java
@@ -8,6 +8,12 @@ public abstract class Unit{
     public int getRange(){return range;}
     public int getDamage(){return damage;}
     
+    public void setSpeed(int s){speed = s;}
+    public void setArmour(int a){armour = a;}
+    public void setRange(int r){range = r;}
+    public void setDamage(int d){damage = d;}
+    
+
     public abstract void move();
 
     public void attack(Unit other){
